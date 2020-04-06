dd7137c1e2b7f8d0cacfc18213a3d72a1adb59f7
ivan <menja08@yahoo.com>
2020-04-07 00:41:12 +0200

    just a clean up commit, commenting out unnecessary code

--- files/js/specificHouse.js
+++ files/js/specificHouse.js
@@ -2,9 +2,9 @@
 console.log(56);
 var specificHouse = document.getElementById("specificHouse");
 var houseInformation = "http://localhost:3000/specificHouse.html/imageName";
-console.log("houseInformation " + houseInformation);
+//console.log("houseInformation " + houseInformation);
 
-specificHouse.append(houseInformation);
+//specificHouse.append(houseInformation);
 
 var houseId = document.getElementById("house");
 var backArrow = document.getElementById("backArrow");
